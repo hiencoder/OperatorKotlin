c50e4e05396ed4cb275699a8c30d5389cd757db4
hiencoder <mrphonglinh2605@gmail.com>
2018-07-10 17:31:21 +0700

    Create ProgressBar

--- app/src/main/java/com/example/hiennv/operator/ProgressBarActivity.kt
+++ app/src/main/java/com/example/hiennv/operator/ProgressBarActivity.kt
@@ -4,7 +4,7 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
 class ProgressBarActivity : AppCompatActivity() {
-
+//https://github.com/MindorksOpenSource/android-mvp-architecture
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_progress_bar)
